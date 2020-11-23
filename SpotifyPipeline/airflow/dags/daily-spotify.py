bc6b917527b73083f2be0265a3756052934b3e8b
Petri Salminen <petri@salminen.dev>
2020-11-23 18:23:21 +0200

    Add count_top_artists and cache to requests in daily

--- SpotifyPipeline/airflow/dags/daily-spotify.py
+++ SpotifyPipeline/airflow/dags/daily-spotify.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.hooks import S3_hook
+from cachier import cachier
 import pandas as pd
 from airflow.operators.python_operator import PythonOperator
 
@@ -21,6 +22,9 @@ def save_to_s3(data, key):
     sthree = get_s3_object()
     return sthree.load_string(data, key, BUCKET_NAME, replace=True)
 
+@cachier()
+def read_csv(key):
+    return pd.read_csv("s3://" + BUCKET_NAME + "/" + key)
 
 default_args = {
     'retries': 0,
@@ -40,19 +44,42 @@ with DAG(
         # We need only the track listing
         keys = [key for key in keys if key.startswith('track_list_')]
         dfs = []
+        if keys == []:
+            return
         for key in keys:
-            dfs.append(pd.read_csv("s3://" + BUCKET_NAME + "/" + key))
+            dfs.append(read_csv(key))
         # Concat the reversed list of dfs. Drop duplicates and reset the index
         # WATCH OUT FOR THE TIME COMPLEXITY OF THIS!!!!
         df = pd.concat(dfs[::-1]).drop_duplicates().reset_index(drop=True)
         df.played_at = pd.to_datetime(df.played_at)
         today_df = df[(df.played_at > context.get('ds')) & (df.played_at < context.get('tomorrow_ds'))]
-        key = "s3://" + BUCKET_NAME + "/daily_listened_tracks_" + context.get('ds') + ".csv"
+        key = "daily_listened_tracks_" + context.get('ds') + ".csv"
         csv_contents = StringIO()
         today_df.to_csv(csv_contents)
         save_to_s3(csv_contents.getvalue(), key)
         print(today_df)
         return key
+
+    def count_top_artists(**context):
+        keys = context['task_instance'].xcom_pull(task_ids='list_s3_objects')
+        # We need only the track listing
+        keys = [key for key in keys if key.startswith('track_list_')]
+        dfs = []
+        if keys == []:
+            return
+        for key in keys:
+            dfs.append(read_csv(key))
+        # Concat the reversed list of dfs. Drop duplicates and reset the index
+        # WATCH OUT FOR THE TIME COMPLEXITY OF THIS!!!!
+        df = pd.concat(dfs[::-1]).drop_duplicates().reset_index(drop=True)
+        df['n'] = 1
+        top_artist_df = df.groupby('artist').n.sum()
+        csv_contents = StringIO()
+        top_artist_df.to_csv(csv_contents)
+        key = "top_artists_" + context.get('ds') + ".csv"
+        save_to_s3(csv_contents.getvalue(), key)
+        return key
+
 
     # Using a custom MultilineBashOperator just to prove that I am aware of
     # operators other than PythonOperator :D
@@ -68,4 +95,10 @@ with DAG(
         provide_context = True,
     )
 
-    list_s3_objects >> daily_listens
+    top_artists = PythonOperator(
+        task_id = 'top_artists',
+        python_callable = count_top_artists,
+        provide_context = True,
+    )
+
+    list_s3_objects >> [daily_listens, top_artists]
